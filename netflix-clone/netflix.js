f465b062c46b02dbe1eac83f6583e61e71311836
vaibhavi tiwari <120113241+vaibhavitiwarii@users.noreply.github.com>
2023-08-22 21:14:35 +0530

    Add files via upload

---
+++ netflix-clone/netflix.js
@@ -0,0 +1,48 @@
+gasp.from(".features .row1", {
+
+    y:700,
+    scrollTrigger:{
+        trigger:".features .row1",
+        scroller: "body",
+        // markers: true,
+        start: "top 130% ",
+    },
+})
+gasp.from(".features .row2",{
+    y:700,
+    scrollTrigger:{
+        trigger:".features .row2",
+        scroller: "body",
+        //markers: true,
+        start: "top 130%",
+    },
+})
+gasp.from(".features .row2",{
+    y:700,
+    scrollTrigger:{
+        trigger:".features .row2",
+        scroller: "body",
+        //markers: true,
+        start: "top 130%",
+    },
+})
+
+gasp.from(".features .row3",{
+    y:700,
+    scrollTrigger:{
+        trigger:".features .row3",
+        scroller: "body",
+        //markers: true,
+        start: "top 130%",
+    },
+})
+gasp.from(".features .row4",{
+    y:700,
+    scrollTrigger:{
+        trigger:".features .row4",
+        scroller: "body",
+        //markers: true,
+        start: "top 130%",
+    },
+})
+
